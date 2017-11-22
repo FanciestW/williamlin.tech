2a3082f70adc4c5ce70fc8dd4cd825b2d8bc3c18
William Lin <wlin26@yahoo.com>
2017-11-22 15:17:14 -0500

    Added website template and made custom changes

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get('/', function(req, res){
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.get('/img/profile_pic.jpg', function(req, res){
+    res.sendFile(path.join(__dirname + '/img/profile_pic.jpg'));
+});
+
 app.listen(PORT, function(){
     console.log("Listening on port " + PORT);
 });
