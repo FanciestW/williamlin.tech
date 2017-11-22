af1d72358ee801f8999e7a32684b911a7a96957e
William Lin <wlin26@yahoo.com>
2017-11-22 15:47:56 -0500

    Added favicon

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ var path = require('path');
 const express = require('express');
 const app = express();
 
-const PORT = 2000;
+const PORT = 80;
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/img/favicon.png', function(req, res){
+    res.sendFile(path.join(__dirname + '/img/favicon.png'));
 });
 
 app.get('/img/profile_pic.jpg', function(req, res){
