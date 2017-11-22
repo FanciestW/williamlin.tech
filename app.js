dce1624563e31e654eccda2a1961b8951b460fa7
William Lin <wlin26@yahoo.com>
2017-11-22 18:21:40 -0500

    Changed port to 80

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 var favicon = require('serve-favicon');
 const app = express();
 
-const PORT = 2000;
+const PORT = 80;
 
 app.use(favicon(__dirname + '/img/favicon.ico'));
 
