ba1264e1c5f0d9fa4f719b4fe3f8aaaeca0cb7a1
William Lin <wlin26@yahoo.com>
2018-02-11 23:20:48 -0500

    Message sending done
    Plan on adding email validation

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 var path = require('path');
 const express = require('express');
 var favicon = require('serve-favicon');
+var bodyParser = require('body-parser');
+const sgMail = require('@sendgrid/mail');
 const app = express();
 
-const PORT = 80;
+const PORT = 8080;
 
 app.use(favicon(__dirname + '/img/favicon.ico'));
+
+app.use(bodyParser.urlencoded({ extended: false })) 
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname + '/index.html'));
@@ -23,6 +29,30 @@ app.get('/img/profile_pic.jpg', function(req, res){
     res.sendFile(path.join(__dirname + '/img/profile_pic.jpg'));
 });
 
+app.post('/', function(req, res) {
+    var senderName = req.body.name;
+    var senderEmail = req.body.email;
+    var senderMessage = req.body.message;
+
+    console.log(senderName);
+    console.log(senderEmail);
+    console.log(senderMessage);
+
+    sendEmailToMe(senderName, senderEmail, senderMessage);
+    res.redirect('/');
+  });
+
 app.listen(PORT, function(){
     console.log("Listening on port " + PORT);
 });
+
+function sendEmailToMe(name, email, message) {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+        to: 'wlin26@yahoo.com',
+        from: email,
+        subject: name + ' - williamlin.tech',
+        text: message
+    };
+    sgMail.send(msg);
+}
--- script/script.js
+++ script/script.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
     $("#emailBtn").on("click", function(){
         $("#sendEmailForm").css('display', 'block');
     });
+
+    $("#btnMessageSend").on("click", function(){
+        $("#emailMessageForm").submit();
+    });
 });
