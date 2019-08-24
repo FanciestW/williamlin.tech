4f83380d46d6da174371f38675971bf23c442dcd
William Lin <wlin26@yahoo.com>
2019-08-24 14:03:32 -0400

    Added form validation

--- script/script.js
+++ script/script.js
@@ -1,13 +1,18 @@
 $(document).ready(function() {
 
-    $("#emailBtn, #btnEmailMe").on("click", openSendEmailModal);
-    $("#btnMessageClose, #sendEmailClose").on("click", closeSendEmailModal);
+    $('#emailBtn, #btnEmailMe').on('click', openSendEmailModal);
+    $('#btnMessageClose, #sendEmailClose').on('click', closeSendEmailModal);
 
     // Send message via sendMeMail API.
-    $("#btnMessageSend").on("click", function(){
-        var emailMessage = $("#emailFormMessage").val();
-        var senderName = $("#emailFormSenderName").val();
-        var senderEmail = $("#emailFormSenderEmail").val();
+    $('#btnMessageSend').on('click', function(){
+        
+        if (!validateSendEmailForm()) {
+            return false;
+        }
+
+        const emailMessage = $('#emailFormMessage').val();
+        const senderName = $('#emailFormSenderName').val();
+        const senderEmail = $('#emailFormSenderEmail').val();
 
         body = JSON.stringify({
             message: emailMessage,
@@ -17,7 +22,7 @@ $(document).ready(function() {
         $.post('https://608y0baaza.execute-api.us-east-1.amazonaws.com/dev/email', body).done(function(data) {
             console.log(data);
         });
-        $("#sendEmailForm").css('display', 'none');
+        $('#sendEmailForm').css('display', 'none');
     });
 
     // When the user clicks anywhere outside of the modal, close it
@@ -29,9 +34,32 @@ $(document).ready(function() {
 });
 
 function openSendEmailModal() {
-    $("#sendEmailForm").css('display', 'block');
+    $('#sendEmailForm').css('display', 'block');
 }
 
 function closeSendEmailModal() {
-    $("#sendEmailForm").css('display', 'none');
+    $('#sendEmailForm').css('display', 'none');
+}
+
+function validateSendEmailForm() {
+    let isValid = true;
+
+    const message = $('#emailFormMessage').val();
+    const senderName = $('#emailFormSenderName').val();
+    const senderEmail = $('#emailFormSenderEmail').val();
+
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    if (!emailRegex.test(senderEmail)) {
+        $('#sendEmailErrorMessage').text('* Invalid Email');
+        isValid &= false;
+    } else {
+        $('#sendEmailErrorMessage').text('');
+    }
+    if (!message) {
+        isValid &= false;
+    }
+    if (!senderName) {
+        isValid &= false;
+    }
+    return isValid;
 }
